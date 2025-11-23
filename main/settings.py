d8fcc23d5abcca636577d3ad756463fe1fe2af37
Sergey Kuznetsov <kyznetsovere@gmail.com>
2025-11-23 18:08:03 +0300

    chore(reset): recreate migrations, clean project state and fix docker config

--- main/settings.py
+++ main/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #---my apps---
+
     'rest_framework',
+
     'products',
     'orders',
     'reviews',
@@ -53,6 +55,7 @@ INSTALLED_APPS = [
     'cart',
     'api',
 
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +73,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
